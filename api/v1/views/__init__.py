f32bd8e37f9fbf341b94fbc4a9c00739eb36833a
SoufianKm <soufian.kamma@gmail.com>
2024-04-27 12:53:54 +0000

    Remove unwanted comments

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""views
-from flask import Blueprint
-app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
-"""
+"""views"""
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
